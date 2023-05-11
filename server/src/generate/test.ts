4baf87e8aaa34f591fc01b5c0108b146f1d72697
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-05-11 15:14:29 +0300

    (feat) - add test data in generation module

---
+++ server/src/generate/test.ts
@@ -0,0 +1,250 @@
+const input = {
+  teacher: "",
+  department: "",
+  year: "",
+  group: ""
+};
+
+// Перша сторінка
+const firstData = {
+  departmentFullName: "Кафедра комп’ютерних інформаційних технологій",
+  departmentCode: 22,
+  departmentCommanderRank: "полковник",
+  departmentCommanderInitialName: "Е",
+  departmentCommanderSecondName: "БОВДА",
+  nowYear: "2023",
+  fromTheYear: "2023",
+  upToYear: "2024",
+  teacherPosition: "доцента кафедри",
+  positionRate: "ставка",
+  teacherDegree: "кандидата технічних наук",
+  teacherRank: "підполковника",
+  teacherSecondName: "ЗДОРЕНКА",
+  teacherFirstName: "Юрія",
+  teacherThirdName: "Миколайовича"
+};
+
+// Друга сторінка
+const secondData = {
+  semester: 1,
+  fromTheYear: "2023",
+  upToYear: "2024",
+  disciplines: [
+    {
+      name: "Операційні системи  (ЗФН)",
+      codeSpecialize: "КН",
+      courseStudy: 2,
+      countOfGroup: 21,
+      groupCode: 29101,
+      totalLectures: 6,
+      totalGroup: 30,
+      totalPractice: 40,
+      verificationLaboratoryWork: 0,
+      conductingTacticalTraining: 0
+    },
+    {
+      name: "Операційні системи  (ЗФН)",
+      codeSpecialize: "КН",
+      courseStudy: 2,
+      countOfGroup: 21,
+      groupCode: 29101,
+      totalLectures: 6,
+      totalGroup: 30,
+      totalPractice: 40,
+      verificationLaboratoryWork: 0,
+      conductingTacticalTraining: 0
+    },
+    {
+      name: "Операційні системи  (ЗФН)",
+      codeSpecialize: "КН",
+      courseStudy: 2,
+      countOfGroup: 21,
+      groupCode: 29101,
+      totalLectures: 6,
+      totalGroup: 30,
+      totalPractice: 40,
+      verificationLaboratoryWork: 0,
+      conductingTacticalTraining: 0
+    }
+  ],
+  semesterLectures: 30,
+  semesterGroup: 60,
+  semesterPractice: 70,
+  semesterVerificationLaboratoryWork: 0,
+  semesterConductingTacticalTraining: 0
+};
+
+// Третя сторінка
+const thirdData = {
+  semester: 1,
+  fromTheYear: "2023",
+  upToYear: "2024",
+  disciplines: [
+    {
+      holdingСonsultations: 0.09,
+      guidelinesDefenseTermPapers: 0,
+      checkingControl: 12.5,
+      conductingExam: 42.5,
+      managementMilitaryInternships: 50,
+      guidanceConsultationReview: 0,
+      defenseCertificationWorks: 0,
+      conductingComprehensiveExaminations: 0,
+      conductingOtherWorks: 0,
+      total: 94.6
+    },
+    {
+      holdingСonsultations: 0.09,
+      guidelinesDefenseTermPapers: 0,
+      checkingControl: 12.5,
+      conductingExam: 42.5,
+      managementMilitaryInternships: 50,
+      guidanceConsultationReview: 0,
+      defenseCertificationWorks: 0,
+      conductingComprehensiveExaminations: 0,
+      conductingOtherWorks: 0,
+      total: 94.6
+    },
+    {
+      holdingСonsultations: 0.09,
+      guidelinesDefenseTermPapers: 0,
+      checkingControl: 12.5,
+      conductingExam: 42.5,
+      managementMilitaryInternships: 50,
+      guidanceConsultationReview: 0,
+      defenseCertificationWorks: 0,
+      conductingComprehensiveExaminations: 0,
+      conductingOtherWorks: 0,
+      total: 94.6
+    }
+  ],
+  semesterHoldingСonsultations: 3,
+  semesterGuidelinesDefenseTermPapers: 0,
+  semesterCheckingControl: 12.5,
+  semesterConductingExam: 42.5,
+  semesterManagementMilitaryInternships: 50,
+  semesterGuidanceConsultationReview: 0,
+  semesterDefenseCertificationWorks: 0,
+  semesterConductingComprehensiveExaminations: 0,
+  semesterConductingOtherWorks: 0,
+  semesterTotal: 94.6
+};
+
+// Четверта сторінка
+const fourthData = {
+  semester: 2,
+  fromTheYear: "2024",
+  upToYear: "2025",
+  disciplines: [
+    {
+      name: "Операційні системи  (ЗФН)",
+      codeSpecialize: "КН",
+      courseStudy: 2,
+      countOfGroup: 21,
+      groupCode: 29101,
+      totalLectures: 6,
+      totalGroup: 30,
+      totalPractice: 40,
+      verificationLaboratoryWork: 0,
+      conductingTacticalTraining: 0
+    },
+    {
+      name: "Операційні системи  (ЗФН)",
+      codeSpecialize: "КН",
+      courseStudy: 2,
+      countOfGroup: 21,
+      groupCode: 29101,
+      totalLectures: 6,
+      totalGroup: 30,
+      totalPractice: 40,
+      verificationLaboratoryWork: 0,
+      conductingTacticalTraining: 0
+    },
+    {
+      name: "Операційні системи  (ЗФН)",
+      codeSpecialize: "КН",
+      courseStudy: 2,
+      countOfGroup: 21,
+      groupCode: 29101,
+      totalLectures: 6,
+      totalGroup: 30,
+      totalPractice: 40,
+      verificationLaboratoryWork: 0,
+      conductingTacticalTraining: 0
+    }
+  ],
+  semesterLectures: 30,
+  semesterGroup: 60,
+  semesterPractice: 70,
+  semesterVerificationLaboratoryWork: 0,
+  semesterConductingTacticalTraining: 0,
+  yearLectures: 190,
+  yearGroup: 260,
+  yearPractice: 170,
+  yearVerificationLaboratoryWork: 0,
+  yearConductingTacticalTraining: 0,
+};
+
+// П'ята сторінка
+const fifthData = {
+  semester: 2,
+  fromTheYear: "2024",
+  upToYear: "2025",
+  disciplines: [
+    {
+      holdingСonsultations: 0.09,
+      guidelinesDefenseTermPapers: 0,
+      checkingControl: 12.5,
+      conductingExam: 42.5,
+      managementMilitaryInternships: 50,
+      guidanceConsultationReview: 0,
+      defenseCertificationWorks: 0,
+      conductingComprehensiveExaminations: 0,
+      conductingOtherWorks: 0,
+      total: 94.6
+    },
+    {
+      holdingСonsultations: 0.09,
+      guidelinesDefenseTermPapers: 0,
+      checkingControl: 12.5,
+      conductingExam: 42.5,
+      managementMilitaryInternships: 50,
+      guidanceConsultationReview: 0,
+      defenseCertificationWorks: 0,
+      conductingComprehensiveExaminations: 0,
+      conductingOtherWorks: 0,
+      total: 94.6
+    },
+    {
+      holdingСonsultations: 0.09,
+      guidelinesDefenseTermPapers: 0,
+      checkingControl: 12.5,
+      conductingExam: 42.5,
+      managementMilitaryInternships: 50,
+      guidanceConsultationReview: 0,
+      defenseCertificationWorks: 0,
+      conductingComprehensiveExaminations: 0,
+      conductingOtherWorks: 0,
+      total: 94.6
+    }
+  ],
+  semesterHoldingСonsultations: 3,
+  semesterGuidelinesDefenseTermPapers: 0,
+  semesterCheckingControl: 12.5,
+  semesterConductingExam: 42.5,
+  semesterManagementMilitaryInternships: 50,
+  semesterGuidanceConsultationReview: 0,
+  semesterDefenseCertificationWorks: 0,
+  semesterConductingComprehensiveExaminations: 0,
+  semesterConductingOtherWorks: 0,
+  semesterTotal: 94.6,
+  yearHoldingСonsultations: 3,
+  yearGuidelinesDefenseTermPapers: 0,
+  yearCheckingControl: 12.5,
+  yearConductingExam: 42.5,
+  yearManagementMilitaryInternships: 50,
+  yearGuidanceConsultationReview: 0,
+  yearDefenseCertificationWorks: 0,
+  yearConductingComprehensiveExaminations: 0,
+  yearConductingOtherWorks: 0,
+  yearTotal: 94.6
+};
